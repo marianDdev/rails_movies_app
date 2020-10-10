2740b017e557c1eccb5633b0702e89b1cd5f58b6
Marian Dumitru <d.mariandev@gmail.com>
2020-10-10 22:30:02 +0300

    added title, summary, dscription src   migration

--- db/migrate/20201010192552_create_movies.rb
+++ db/migrate/20201010192552_create_movies.rb
@@ -3,6 +3,10 @@ class CreateMovies < ActiveRecord::Migration[6.0]
     create_table :movies do |t|
 
       t.timestamps
+      t.text :title
+      t.text :summary
+      t.text :description
+      t.text :img_src
     end
   end
 end
