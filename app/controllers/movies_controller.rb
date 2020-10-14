c2985818f3a10f43e3168c137d9511be92c5b444
Marian Dumitru <d.mariandev@gmail.com>
2020-10-14 23:07:19 +0300

    added new and create actions, movie params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,22 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find params[:id]
    end
+
+   def new 
+    @movie = Movie.new 
+  end
+  
+  def create 
+    @movie = Movie.new(movie_params) 
+    if @movie.save 
+      redirect_to '/movies' 
+    else 
+      render 'new' 
+    end 
+  end 
+
+   private 
+  def movie_params 
+    params.require(:movie).permit(:title, :summary, :description, :img_src) 
+  end 
 end
